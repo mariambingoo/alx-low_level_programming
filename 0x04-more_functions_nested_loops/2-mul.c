18814f307d28533f467771b75f84145f68f6a9db
mariambingoo <mariamsheta13@gmail.com>
2023-07-21 19:58:42 -0700

    0x04

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,11 +1,12 @@
 #include "main.h"
+
 /**
- * mul - multiply 2 int
- * @a: first int
- * @b: second int
- * Return: addition of the multiplicattion
+ * mul - Multiply two integers.
+ * @a: First integer.
+ * @b: Second integer.
+ * Return: The result of the multiplication.
  */
- int mul(int a, int b)
- {
- 	return (a * b);
- }
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,32 +8,33 @@
  */
 int main(void)
 {
-    int i;
+	int i;
 
-    for (i = 1; i <= 100; i++)
-    {
-        if (i % 3 == 0 && i % 5 != 0)
-        {
-            printf(" Fizz");
-        }
-        else if (i % 5 == 0 && i % 3 != 0)
-        {
-            printf(" Buzz");
-        }
-        else if (i % 3 == 0 && i % 5 == 0)
-        {
-            printf(" FizzBuzz");
-        }
-        else if (i == 1)
-        {
-            printf("%d", i);
-        }
-        else
-        {
-            printf(" %d", i);
-        }
-    }
-    printf("\n");
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 != 0)
+		{
+			printf(" Fizz");
+		}
+		else if (i % 5 == 0 && i % 3 != 0)
+		{
+			printf(" Buzz");
+		}
+		else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf(" FizzBuzz");
+		}
+		else if (i == 1)
+		{
+			printf("%d", i);
+		}
+		else
+		{
+			printf(" %d", i);
+		}
+	}
 
-    return (0);
+	printf("\n");
+
+	return (0);
 }
